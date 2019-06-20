ca1ba45f4d69a07254cac05dc0ac2ab403f2288f
bay007 <tnt_code@hotmail.com>
2019-06-20 01:23:31 -0500

    remove boostrap and jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,10 +7,7 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
+	window.Popper = require('popper.js').default;
 } catch (e) {}
 
 /**
@@ -32,9 +29,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
